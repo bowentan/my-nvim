4adf91df1ecc1fc34331185394912bc5a7dc3e84
Bowen Tan <bowentan78@gmail.com>
2026-01-12 22:15:15 +0800

    feat(avante): add poe

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,12 +1,20 @@
 return {
   {
     "yetone/avante.nvim",
-    keys = {
-      {
-        "<leader>aC",
-        ":AvanteClear<CR>",
-        mode = "n",
-        desc = "Clear Avante",
+    opts = {
+      provider = "Poe",
+      providers = {
+        ["Poe"] = {
+          __inherited_from = "openai",
+          api_key_name = "AVANTE_POE_API_KEY",
+          endpoint = "https://api.poe.com/v1",
+          model = "Claude-Sonnet-4.5",
+          extra_request_body = {
+            extra_body = {
+              thinking_budget = 0,
+            },
+          },
+        },
       },
     },
   },
